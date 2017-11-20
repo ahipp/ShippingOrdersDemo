322b8a3e6703d0a4a5f98d8483f352f36c39c078
ahipp <hipp.adam@gmail.com>
2017-11-19 19:02:53 -0600

    Add nav bar

--- Main/Scripts/app/app.component.ts
+++ Main/Scripts/app/app.component.ts
@@ -1,9 +1,21 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-app',
-  template: '<router-outlet></router-outlet>' }) 
-export class AppComponent implements OnInit { 
-    ngOnInit() { console.log('AppInit!'); }
-}
+  template: `
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" [routerLink]="['/home']">Adam Hipp's Assessment</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a [routerLink]="['/user/new']">Create new user</a></li>
+                <li><a [routerLink]="['/order/new']">Create new order</a></li>
+                <li><a [routerLink]="['/user-orders']">View/Edit Orders</a></li>
+            </ul>
+        </div>
+    </nav>
+    <router-outlet></router-outlet>
+  ` }) 
+export class AppComponent { }
